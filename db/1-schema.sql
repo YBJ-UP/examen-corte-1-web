9dbd9dd9bdd5bea580a54eab648802bbc880fb0e
Yael <243678@ids.upchiapas.edu.mx>
2026-02-03 18:37:43 -0600

    solo le faltan los indices

--- db/1-schema.sql
+++ db/1-schema.sql
@@ -52,7 +52,14 @@ CREATE TABLE calificaciones ( --calificaciones de 0 a 100
     id_inscripcion INTEGER NOT NULL REFERENCES inscripciones(id),
     parcial_1 INTEGER CHECK (parcial_1 >= 0 AND parcial_1 <= 100),
     parcial_2 INTEGER CHECK (parcial_2 >= 0 AND parcial_2 <= 100),
-    final INTEGER CHECK (final >= 0 AND final <= 100),
+    final INTEGER CHECK (final >= 0 AND final <= 100)
+);
+
+CREATE TABLE asistencia (
+    id SERIAL PRIMARY KEY,
+    id_inscripcion INTEGER NOT NULL REFERENCES inscripciones(id),
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    asistencia BOOLEAN
 );
 
 -- ESTUDIANTES HASTA ACÁ
