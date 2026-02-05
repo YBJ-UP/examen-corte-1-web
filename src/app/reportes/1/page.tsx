8cc37c61b1f8d4f013cc98123df31102752033fb
Yael <243678@ids.upchiapas.edu.mx>
2026-02-05 17:04:01 -0600

    Pruebas de conexión con el front (sí funcionó)

--- src/app/reportes/1/page.tsx
+++ src/app/reportes/1/page.tsx
@@ -1,6 +1,15 @@
 import Construyendo from "@/components/construccion"
+import { query } from "@/lib/db"
 
-export default function reporte1 () {
+export const dynamic = 'force-dynamic';
+
+export default async function reporte1 () {
+    const view = await query('SELECT * FROM vw_rendimiento_curso');
+    if (!view.rows) {
+        throw new Error('jjjjjjj');
+    }
+    const reporte = view.rows;
+    console.log(reporte);
     return (
         <div>
             <Construyendo />
