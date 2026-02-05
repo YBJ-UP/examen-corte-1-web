af90f1de3d1385f7d11411eb9fc3d9ef8c4e84d9
Yael <243678@ids.upchiapas.edu.mx>
2026-02-04 22:47:29 -0600

    ps resulta que esos van y no saque el promedio de la suma

--- db/4-reports_vw.sql
+++ db/4-reports_vw.sql
@@ -7,13 +7,13 @@ CREATE OR REPLACE VIEW vw_rendimiento_curso AS
         c.nombre as curso,
         g.periodo,
         e.programa,
-        ROUND((ca.parcial_1 + ca.parcial_2 + ca.final)/3.0,2) AS promedio,
+        ROUND(AVG((ca.parcial_1 + ca.parcial_2 + ca.final)/3.0),2) AS promedio,
         COUNT(CASE WHEN ((ca.parcial_1 + ca.parcial_2 + ca.final)/3.0 > 70) THEN 1 END) AS reprobados
     FROM cursos c
     JOIN grupos g ON c.id = g.id_curso
     JOIN inscripciones i ON i.id_grupo = g.id
     JOIN calificaciones ca ON ca.id_inscripcion = i.id
     JOIN estudiantes e ON e.id = i.id_estudiante
-    GROUP BY c.nombre;
+    GROUP BY c.nombre, g.periodo, e.programa;
 
 -- toma curso y periodo, programa pq lo pide y calificaciones pq la view es de calificaciones, las inscripciones pq las calificaciones van ligadas a esas
