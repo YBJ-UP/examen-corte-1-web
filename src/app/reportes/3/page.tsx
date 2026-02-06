a81d2486fa34a73741c9e7f29273e6dd58df47f3
Yael <243678@ids.upchiapas.edu.mx>
2026-02-06 07:44:51 -0600

    cambios a la semilla para hacer pruebas de paginacion, se medio termina la vista del reporte 3

--- src/app/reportes/3/page.tsx
+++ src/app/reportes/3/page.tsx
@@ -1,9 +1,42 @@
-import Construyendo from "@/components/construccion"
+import Paginacion from "@/components/paginacion"
+import { paginaSchema } from "@/shared/interfaces/pagina"
+import { paginarRep3, reporte3Type } from "@/shared/interfaces/reporte_3"
 
-export default async function reporte_3 () {
+export const dynamic = 'force-dynamic'
+
+export default async function reporte_3 ({ searchParams }: { searchParams:{[key:string]: string} }) {
+    const paginaUnparsed = await searchParams
+        const paginaParsed = paginaSchema.parse(paginaUnparsed)
+        const res = await paginarRep3(paginaParsed)
+        if (!res.ok) {
+            throw new Error(res.mensaje)
+        }
+        const { data, pagination } = res
     return (
         <div>
-            <Construyendo />
+            <div className="m-10">
+                        <h1 className="text-2xl font-bold">Alumnos preocupantes</h1>
+                        <p>Alumnos con calificaciones bajas o pocas asistencias.</p>
+            
+                        <div className="my-5">
+                            <div className="grid grid-cols-5 items-center border-2 border-amber-50 p-2">
+                                <p>Nombre del alumno</p>
+                                <p>Correo electrónico</p>
+                                <p>Promedio de calificaciones</p>
+                                <p>Promedio de asistencias</p>
+                            </div>
+                            {data.map((dato:reporte3Type, key:number) => (
+                                <div key={key} className="grid grid-cols-5 items-center border-2 border-amber-50 p-2">
+                                    <p>{dato.nombre}</p>
+                                    <p>{dato.correo}</p>
+                                    <p>{dato.promedio_calificaciones}</p>
+                                    <p>{dato.promedio_asistencias}</p>
+                                </div>
+                            ))}
+                        </div>
+            
+                        <Paginacion paginaActual={pagination.pagina} paginasTotales={pagination.totalPaginas} ruta="/reportes/2"/>
+                    </div>
         </div>
     )
 }
