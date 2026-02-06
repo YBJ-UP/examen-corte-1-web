45063a76be77960f18e5ec627afd11dde5090713
Yael <243678@ids.upchiapas.edu.mx>
2026-02-05 19:49:39 -0600

    no le habia dado permisos xd

--- db/6-roles.sql
+++ db/6-roles.sql
@@ -10,4 +10,8 @@ INHERIT;
 
 GRANT USAGE ON SCHEMA public TO app;
 GRANT SELECT ON vw_rendimiento_curso TO app;
+GRANT SELECT ON vw_carga_maestro TO app;
+GRANT SELECT ON vw_estudiantes_preocupantes TO app;
+GRANT SELECT ON vw_asistencia_por_grupo TO app;
+GRANT SELECT ON vw_tablero_estudiantes TO app;
 -- dar permiso para los reportes (cuando los haga)
