5ec6d290b3b92861b90258f992e0ec65fda4c1b9
Yael <243678@ids.upchiapas.edu.mx>
2026-02-06 01:02:11 -0600

    eventualmente va a funcionar, de todos modos que es una recu mas

--- src/app/reportes/2/page.tsx
+++ src/app/reportes/2/page.tsx
@@ -4,15 +4,15 @@ import Paginacion from "@/components/paginacion"
 
 export const dynamic='force-dynamic'
 
-export default async function reporte_2 ({ searchParams }: { searchParams:number }) {
+export default async function reporte_2 ({ searchParams }: { searchParams:string }) {
     const paginaUnparsed = await searchParams
-    const paginaParsed = paginaSchema.parse(paginaUnparsed)
+    let paginaParsed = paginaSchema.parse(paginaUnparsed)
+    console.log(paginaUnparsed, paginaParsed)
     const res = await paginarRep2(paginaParsed)
     if (!res.ok) {
         throw new Error('Error al conseguir los reportes')
     }
     const { data, pagination } = await res.json()
-    console.log(data)
     return (
         <div>
             <h1 className="text-2xl font-bold">Carga del maestro</h1>
--- src/components/paginacion.tsx
+++ src/components/paginacion.tsx
@@ -9,13 +9,12 @@ interface paginaInterfaz {
 export default async function paginacion({ paginaActual, paginasTotales, ruta }:paginaInterfaz) {
     const primeraPagina: boolean = paginaActual == 1
     const ultimaPagina: boolean = paginaActual == paginasTotales
-    console.log(paginaActual, paginasTotales, ruta, primeraPagina, ultimaPagina)
     return (
         <div className="flex flex-row justify-center gap-6">
             {
                 !primeraPagina && <Link href={`${ruta}?page=${paginaActual-1}`}>Página anterior</Link>
             }
-            <p>{paginaActual}</p>
+            <p>{paginaActual} de {paginasTotales}</p>
             {
                 !ultimaPagina && <Link href={`${ruta}?page=${paginaActual+1}`}>Siguiente página</Link>
             }
