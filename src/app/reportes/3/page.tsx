dcdf5e5835eb020eb66885cffc8db7f57f78a130
Yael <243678@ids.upchiapas.edu.mx>
2026-02-11 16:25:55 -0600

    estaba rara la indentacion

--- src/app/reportes/3/page.tsx
+++ src/app/reportes/3/page.tsx
@@ -22,28 +22,28 @@ export default async function reporte_3 ({ searchParams }: { searchParams:{[key:
                 <Busqueda />
             </Suspense>
             <div className="m-10">
-                        <h1 className="text-2xl font-bold">Alumnos preocupantes</h1>
-                        <p>Alumnos con calificaciones bajas o pocas asistencias.</p>
-            
-                        <div className="my-5">
-                            <div className="grid grid-cols-5 items-center border-2 border-amber-50 p-2">
-                                <p>Nombre del alumno</p>
-                                <p>Correo electrónico</p>
-                                <p>Promedio de calificaciones</p>
-                                <p>Promedio de asistencias</p>
-                            </div>
-                            {data.map((dato:reporte3Type, key:number) => (
-                                <div key={key} className="grid grid-cols-5 items-center border-2 border-amber-50 p-2">
-                                    <p>{dato.nombre}</p>
-                                    <p>{dato.correo}</p>
-                                    <p>{dato.promedio_calificaciones}</p>
-                                    <p>{dato.promedio_asistencias}</p>
-                                </div>
-                            ))}
-                        </div>
-            
-                        <Paginacion paginaActual={pagination.pagina} paginasTotales={pagination.totalPaginas} ruta="/reportes/3"/>
+                <h1 className="text-2xl font-bold">Alumnos preocupantes</h1>
+                <p>Alumnos con calificaciones bajas o pocas asistencias.</p>
+
+                <div className="my-5">
+                    <div className="grid grid-cols-5 items-center border-2 border-amber-50 p-2">
+                        <p>Nombre del alumno</p>
+                        <p>Correo electrónico</p>
+                        <p>Promedio de calificaciones</p>
+                        <p>Promedio de asistencias</p>
                     </div>
+                    {data.map((dato:reporte3Type, key:number) => (
+                        <div key={key} className="grid grid-cols-5 items-center border-2 border-amber-50 p-2">
+                            <p>{dato.nombre}</p>
+                            <p>{dato.correo}</p>
+                            <p>{dato.promedio_calificaciones}</p>
+                            <p>{dato.promedio_asistencias}</p>
+                        </div>
+                    ))}
+                </div>
+
+                <Paginacion paginaActual={pagination.pagina} paginasTotales={pagination.totalPaginas} ruta="/reportes/3"/>
+            </div>
         </div>
     )
 }
