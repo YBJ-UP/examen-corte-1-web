cc8984545b476e355dce22b5c2b432c65c204a56
Yael <243678@ids.upchiapas.edu.mx>
2026-02-11 21:19:50 -0600

    no se por que al utilizar el useState selecciona el anterior y no el recientemente seleccionado asi q se va se empieza a poner el filtrado por periodo

--- src/components/filtrado.tsx
+++ src/components/filtrado.tsx
@@ -4,15 +4,12 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useState } from "react"
 
 export default function filtrado ({programas}:{programas:{programa:string}[]}) {
-    const [prog, setProg] = useState<string>('')
-
     const parametros = useSearchParams()
     const params = new URLSearchParams(parametros)
     const ruta = usePathname()
     const { replace } = useRouter()
 
-    function selecProg(input:string) {
-        setProg(input)
+    function manejarPrograma(input:string) {
         if (input){
             params.set("programa", input)
         } else {
@@ -22,11 +19,17 @@ export default function filtrado ({programas}:{programas:{programa:string}[]}) {
         replace(`${ruta}?${params.toString()}`)
     }
 
+    function manejarPeriodo(input:string) {
+        if (input) {
+            params.set('periodo', input)
+        }
+    }
+
     return (
         <div className="flex gap-5 my-5">
             <div className="flex gap-5 items-center">
                 <label htmlFor="programa">Seleccionar programa:</label>
-                <select name="programa" id="programa" className="bg-black border-2 rounded-2xl px-5 py-2" onChange={(e) => {selecProg(e.target.value.trim())}}>
+                <select name="programa" id="programa" className="bg-black border-2 rounded-2xl px-5 py-2" onChange={(e) => {manejarPrograma(e.target.value.trim())}}>
                     <option value="">-</option>
                     {programas.map((programa, key:number) => (
                         <option key={key} value={programa.programa}>{programa.programa}</option>
