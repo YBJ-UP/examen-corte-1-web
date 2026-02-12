e7be46f9b261c2b3f3ff45684a97275a1c7b4830
Yael <243678@ids.upchiapas.edu.mx>
2026-02-11 23:34:51 -0600

    otra vez la indentacion

--- src/app/reportes/4/page.tsx
+++ src/app/reportes/4/page.tsx
@@ -8,16 +8,16 @@ export default async function reporte_4 () {
     return (
         <div className="flex flex-col m-10">
             <div className="flex gap-20 items-center mb-5">
-                    <div>
-                        <h1 className="text-2xl font-bold">Asistencias por grupo</h1>
-                        <p>Porcentaje de asistencias por cada grupo.</p>
-                    </div>
-                    
-                    <div className=" flex flex-col gap-2 items-center rounded-2xl text-xl bg-red-700 p-5">
-                        <h2 className="font-medium">Grupo con menos asistencias:</h2>
-                        <p className="text-sm">Grupo {kpi.grupo} del periodo {kpi.periodo} con un {kpi.promedio_asistencias}% de asistencias</p>
-                    </div>
+                <div>
+                    <h1 className="text-2xl font-bold">Asistencias por grupo</h1>
+                    <p>Porcentaje de asistencias por cada grupo.</p>
                 </div>
+                
+                <div className=" flex flex-col gap-2 items-center rounded-2xl text-xl bg-red-700 p-5">
+                    <h2 className="font-medium">Grupo con menos asistencias:</h2>
+                    <p className="text-sm">Grupo {kpi.grupo} del periodo {kpi.periodo} con un {kpi.promedio_asistencias}% de asistencias</p>
+                </div>
+            </div>
 
             <div className="grid grid-cols-5 items-center border-2 border-amber-50 p-2">
                 <p>Grupo</p>
